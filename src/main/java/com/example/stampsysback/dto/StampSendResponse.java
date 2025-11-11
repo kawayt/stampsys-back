3fc1baacfd546b1884e1c356eceb2c0a9c5f13e9
Iwasaki1209 <n2420109@ngo.ohara.ac.jp>
2025-11-11 13:59:36 +0900

    add: StampSendController.java, StampSendRequest.java, StampSendResponse.java

--- src/main/java/com/example/stampsysback/dto/StampSendResponse.java
+++ src/main/java/com/example/stampsysback/dto/StampSendResponse.java
@@ -1,12 +1,11 @@
 package com.example.stampsysback.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class StampSendResponse {
     private String status;
     private String message;
-
-    // getters / setters
-    public String getStatus() { return status; }
-    public void setStatus(String status) { this.status = status; }
-    public String getMessage() { return message; }
-    public void setMessage(String message) { this.message = message; }
 }
