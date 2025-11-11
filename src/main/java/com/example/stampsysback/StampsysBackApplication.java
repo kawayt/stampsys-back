78ae956f1a558330c3bb40b56b63ba8d0f6fc198
kawayt <N2420229@ngo.ohara.ac.jp>
2025-11-11 11:47:42 +0900

    initial commit

---
+++ src/main/java/com/example/stampsysback/StampsysBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.stampsysback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StampsysBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StampsysBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stampsysback/StampsysBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stampsysback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StampsysBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
