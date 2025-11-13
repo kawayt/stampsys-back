b2a41e85fa309448df55857e641b131d3fe4dbb0
Iwasaki1209 <n2420109@ngo.ohara.ac.jp>
2025-11-13 09:42:44 +0900

    UPDATE：csrfの無効化

--- src/main/java/com/example/stampsysback/config/SecurityConfig.java
+++ src/main/java/com/example/stampsysback/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 public class SecurityConfig {
@@ -19,10 +18,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-//              .csrf(csrf -> csrf.disable())//元々の設定ではcsrfを無効にしていた
-                .csrf(csrf -> csrf
-                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                )
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/", "/login**", "/error", "/actuator/**", "/api/test-create").permitAll()
                         .anyRequest().authenticated()
