83fc6bcae365290f9690214ef84b1581c06e4e38
Iwasaki1209 <n2420109@ngo.ohara.ac.jp>
2025-11-14 14:07:54 +0900

    UPDATE: スタンプ集計表示機能の修正（スタンプの割合表示の修正）

--- src/main/java/com/example/stampsysback/dto/StampSummary.java
+++ src/main/java/com/example/stampsysback/dto/StampSummary.java
@@ -3,6 +3,8 @@ package com.example.stampsysback.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 @Getter
 @Setter
 public class StampSummary {
@@ -10,4 +12,6 @@ public class StampSummary {
     private String stampName;
     private Integer stampColor;
     private Integer stampIcon;
+    private Integer cnt;
+    private BigDecimal pct;
 }
